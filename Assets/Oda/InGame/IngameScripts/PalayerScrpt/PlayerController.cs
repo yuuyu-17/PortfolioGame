71d6bded33637fd17fa2533c79e78e27cbfd6d57
yuuyu-17 <20122017@kaishi-pu.ac.jp>
2025-01-30 14:23:30 +0900

    プレイヤーのWASDキーでの移動を追加

--- Assets/Oda/InGame/IngameScripts/PalayerScrpt/PlayerController.cs
+++ Assets/Oda/InGame/IngameScripts/PalayerScrpt/PlayerController.cs
@@ -4,23 +4,29 @@ namespace InGame
 {
     public class PlayerController : MonoBehaviour
     {
-       [SerializeField]
-       private float _speed = 0f;
+          private Rigidbody _rb;
+          [SerializeField]
+          private float speed = 3.0f;
 
-       private Rigidbody _rb;
+          void Start()
+          {
+               _rb = GetComponent<Rigidbody> ();
+          }
 
-       private void Start()
-       {
-            _rb = GetComponent<Rigidbody>();
-       }
-
-       private void FixedUpdate()
-       {
-            float moveX = Input.GetAxis("Horizontal");
-            float moveZ = Input.GetAxis("Vertical");
-
-            Vector3 move = new Vector3(moveX, 0, moveZ).normalized * _speed;
-            _rb.linearVelocity = new Vector3(move.x, _rb.linearVelocity.y, move.z);
-       }
+          void Update()
+          {
+               if(Input.GetKey(KeyCode.W)){
+                    _rb.linearVelocity = transform.forward*speed;
+               }
+               if(Input.GetKey(KeyCode.S)){
+                    _rb.linearVelocity = -transform.forward*speed;
+               }
+               if(Input.GetKey(KeyCode.D)){
+                    _rb.linearVelocity = transform.right*speed;
+               }
+               if(Input.GetKey(KeyCode.A)){
+                    _rb.linearVelocity = -transform.right*speed;
+               }
+          }
     }
 }
